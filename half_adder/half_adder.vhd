5393ab2303495887488390f76943424f3263fa62
C. (Bruno) Almeida <cauebalmeida@gmail.com>
2026-04-21 12:06:51 -0300

    Refactor half adder implementation
    Refactor half adder logic to use conditional signals for Sigma and Cout.

--- half_adder/half_adder.vhd
+++ half_adder/half_adder.vhd
@@ -7,7 +7,19 @@ ENTITY half_adder IS
 END half_adder;
 
 ARCHITECTURE df OF half_adder IS
+	
+	# Two bits	
+	SIGNAL AB: std_logic_vector(1 down to 0)
+
 	BEGIN
 		Sigma = A XOR B;
 		Cout = A AND B;
+		
+		# Applying conditional signals
+		Sigma <= '0' WHEN A = B 
+			else '1'
+		
+		AB <= A AND B;
+		Cout <= '1' WHEN AB == '11' 
+			else '0';
 	END df;
